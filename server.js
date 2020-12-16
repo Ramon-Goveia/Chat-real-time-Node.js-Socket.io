fba2d37f5baafc3a934c88cc62a8a6ab21f3b85d
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-12-16 10:13:43 -0300

    server config - protocolo http e wss

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require(path)
+
+const app = express()
+const server = require('http').createServer(app)//protocolo http
+const io = require('socket.io')(server)//protocolo wss
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('views engine', 'html')
+
+app.use('/', (req, res) =>{
+    res.render('index.html')
+})
+
+server.listen(3000)
+
